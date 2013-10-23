e9ed2e241062c91a3676307891ae380bae2d8bde
Haotian He <haotianh@uw.edu>
2013-10-22 18:01:12 -0700

    personality

---
+++ Personality.java
@@ -0,0 +1,23 @@
+// Haotian He
+// 3/5/13
+// CSE 142
+// TA: Torin Blankensmith
+// Assignment #7
+//
+// This program processes a input file of answers to the Keirsey
+
+ 
+import java.awt.*;
+import java.util.*;
+
+public class Bear extends Critter {
+   public Color getColor() {
+	   if () {
+		   return Color.WHITE;
+		}
+		return Color.BLACK;
+	}
+	
+	public String toString() {
+	   return 
+		
